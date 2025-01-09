5b354753f556cbf5d65c208e6e35dc12699663d5
gguilherme42 <gguilherme.rosa42@gmail.com>
2025-01-09 17:45:50 -0300

    improviment and filtering only some files

--- app/main.js
+++ app/main.js
@@ -1,6 +1,32 @@
 const {app, BrowserWindow, dialog} = require("electron");
 const fs = require("fs");
 
+const getFileFromUser = () => {
+    return dialog.showOpenDialog(
+        {
+            properties: ["openFile"],
+            filters: [
+                {name: "Text Files", extensions: ["txt"]},
+                {name: "Markdown Files", extensions: ["md", "markdonw"]}
+
+            ]
+        }
+    )
+        .then((response) => {
+            if (!response.filePaths) return "";
+
+            console.log(fs.readFileSync(response.filePaths[0]).toString());
+            return response.filePaths[0];
+        });
+    
+    //if (!files) return;
+    // const file = files[0];
+    // const content = fs.readFileSync(file).toString();
+    // console.log(`Teste: ${files}`);
+    // console.log(files[0]);  -- the book suggest this code, but it doesn't work properly because "dialog.showOpenDialog" returns a promise object.
+}
+
+
 
 let mainWindow = null;
 
@@ -22,19 +48,3 @@ app.on("ready", () => {
     });
 });
 
-const getFileFromUser = () => {
-    const files = dialog.showOpenDialog({properties: ["openFile"]})
-        .then((response) => {
-            if (!response.filePaths) {console.log("Nada foi selecionado!")};
-            console.log(`Teste: ${response.filePaths}`);
-
-        });
-    
-    //if (!files) return;
-
-    // const file = files[0];
-    // const content = fs.readFileSync(file).toString();
-
-    // console.log(`Teste: ${files}`);
-    // console.log(files[0]);
-}
